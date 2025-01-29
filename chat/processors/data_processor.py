7377a0163efd8165270c41e6ccbe71c5d7983146
vika <vika@granadzer.com>
2025-01-29 03:38:42 +0200

    Retriever dialog

--- chat/processors/data_processor.py
+++ chat/processors/data_processor.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
+
 def get_data(path='chat/data/datasets/updated_cocktails.csv'):
+    '''
+    :param path: path to dataset for info retrieval
+    :return: cocktails info as array
+    '''
     df = pd.read_csv(path)
     cocktails_info_column = df['Combined']
     return cocktails_info_column.values.tolist()
